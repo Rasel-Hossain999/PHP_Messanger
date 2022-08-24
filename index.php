1f12e28001557aac587e57aabf4301811dc218f9
hossainmdrasel999@gmail.com <Rasel-Hossain999>
2022-08-24 11:20:02 -0700

    login form css implement

--- index.php
+++ index.php
@@ -5,11 +5,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ajax basic</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>Massenger</title>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"> -->
 </head>
 
 <body>
+    <div class="signup-container">
+        <div class="account-left">
+            <h1>account left</h1>
+        </div><!-- close account-left -->
+
+        <div class="account">
+            <div class="form">
+                <form method="POST" action="">
+                    <div class="group">
+                        <input type="text" name="full_name" class="control" placeholder="Enter Full Name">
+                    </div> <!-- close group -->
+                    <div class="group">
+                        <input type="email" name="email" class="control" placeholder="Enter Email...">
+                    </div>
+                    <div class="group">
+                        <input type="password" name="password" class="control" placeholder="Enter Password">
+                    </div>
+                    <div class="group">
+                        <label for="file" id="file-label">Choose image</label>
+                        <input type="file" name="img" class="file" id="file">
+                    </div>
+                    <div class="group">
+                        <input type="submit" name="signup" class="btn signup-btn" value="Create Account">
+                    </div>
+                </form>
+            </div><!-- close form area -->
+        </div><!--close account-right -->
+
+    </div> <!--close signup-container -->
 
 
 
